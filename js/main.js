bb9d9f7411c571f2a3348de3d69b32db7c310934
Igor <65125429+DevilDetko@users.noreply.github.com>
2021-02-22 23:50:18 +0200

    Messi
    Messi

---
+++ js/main.js
@@ -0,0 +1,6 @@
+function menuToggle() {
+  const nav = document.getElementById("nav");
+  nav.classList.toggle("active");
+  const toggle = document.getElementById("toggle");
+  toggle.classList.toggle("active");
+}
